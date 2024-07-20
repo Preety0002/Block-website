22aa0725eb9bf153fdc624173c699355eb6b65dd
Preety0002 <prityarju0002@gmail.com>
2024-07-20 19:53:36 +0530

    updated

--- Models/connect.js
+++ Models/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 exports.connectionDB = ()=>{
     try {
-        mongoose.connect("mongodb://0.0.0.0/passport")
+        mongoose.connect("mongodb+srv://arjuprity0002:123preetyy@cluster0.fsls1py.mongodb.net/onetoonechat?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Database Connected");
     } catch (error) {
         console.log(error);
